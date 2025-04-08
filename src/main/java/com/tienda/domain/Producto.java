256978c2d6cd48fa34e03c766c9254c49080c351
DanielS618 <seguradaniel618@gmail.com>
2025-04-07 23:57:05 -0600

    semana 9

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda.domain;
 
-/**
- *
- * @author segur
- */
-public class Producto {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data  //generar por dejabo los set y get
+@Entity
+@Table(name = "producto") 
+public class Producto implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+    private Long idCategoria; //id_categoria
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private String rutaImagen;
+    private boolean activo;
+
+    public Producto(Long idCategoria, String detalle, double precio, int existencias, String rutaImagen, boolean activo) {
+        this.idCategoria = idCategoria;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
+    
     
 }

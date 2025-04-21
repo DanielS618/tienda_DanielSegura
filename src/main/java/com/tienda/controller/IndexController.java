985144b607de6196bda8f16b4056e59a6fd628cb
DanielS618 <seguradaniel618@gmail.com>
2025-04-20 22:45:27 -0600

    cossas que me hacen falta de semanas atras

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -15,7 +15,7 @@ public class IndexController {
         model.addAttribute("attribute", "value");
         return "index";
     }
-@RequestMapping("/informacion") //este es el URL para llamar a la pagina y en el return la pagina que va a mostrar
+    @RequestMapping("/informacion") //este es el URL para llamar a la pagina y en el return la pagina que va a mostrar
     public String contacto(Model model) {
         model.addAttribute("attribute", "value");
         return "contacto";
